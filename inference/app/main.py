b924f61261eda9b1270c4f3263e7a4cc31647307
harrisonpim <harrison.stroma@gmail.com>
2020-04-21 11:55:56 +0100

    add mesh endpoint

--- inference/app/main.py
+++ inference/app/main.py
@@ -3,7 +3,7 @@ import logging
 
 from fastapi import FastAPI, HTTPException
 
-from .src.aggregate import aggregate_lc_names_data
+from .src.aggregate import aggregate_lc_names_data, aggregate_mesh_data
 
 logger = logging.getLogger(__name__)
 
@@ -17,7 +17,7 @@ logger.info("API started, awaiting requests")
 
 
 @app.get("/lc-names/{lc_names_id}")
-def main(lc_names_id: str):
+def lc_names_endpoint(lc_names_id: str):
     try:
         response = aggregate_lc_names_data(lc_names_id)
     except ValueError as e:
@@ -26,6 +26,19 @@ def main(lc_names_id: str):
         raise HTTPException(status_code=404, detail=error_string)
 
     logger.info(f"aggregated concept data for lc_names ID: {lc_names_id}")
+    return response
+
+
+@app.get("/mesh/{mesh_id}")
+def mesh_endpoint(mesh_id: str):
+    try:
+        response = aggregate_mesh_data(mesh_id)
+    except ValueError as e:
+        error_string = str(e)
+        logger.error(error_string)
+        raise HTTPException(status_code=404, detail=error_string)
+
+    logger.info(f"aggregated concept data for lc_names ID: {mesh_id}")
     return response
 
 
--- inference/app/src/aggregate.py
+++ inference/app/src/aggregate.py
@@ -11,12 +11,12 @@ log = logging.getLogger(__name__)
 def aggregate_lc_names_data(lc_names_id):
     lc_names_data = get_lc_names_data(lc_names_id)
     log.info(f"Got data from lc_names for ID: {lc_names_id}")
+
     try:
         wikidata_id = lc_names_id_to_wikidata_id(lc_names_id)
         log.info(
             f'Found a link from lc_names ID: {lc_names_id} to wikidata ID: {wikidata_id}'
         )
-
         wikidata_data = get_wikidata_data(wikidata_id)
         log.info(f"Got data from wikidata for ID: {lc_names_id}")
     except ValueError:
@@ -30,19 +30,19 @@ def aggregate_lc_names_data(lc_names_id):
             "lc_names": lc_names_data,
             "wikidata": wikidata_data
         },
-        "inferred": {}  # TODO
+        "inferred": {}
     }
 
 
 def aggregate_mesh_data(mesh_id):
     mesh_data = get_mesh_data(mesh_id)
     log.info(f"Got data from MeSH for ID: {mesh_id}")
+
     try:
         wikidata_id = mesh_id_to_wikidata_id(mesh_id)
         log.info(
             f'Found a link from mesh ID: {mesh_id} to wikidata ID: {wikidata_id}'
         )
-
         wikidata_data = get_wikidata_data(wikidata_id)
         log.info(f"Got data from wikidata for ID: {mesh_id}")
     except ValueError:
@@ -56,5 +56,5 @@ def aggregate_mesh_data(mesh_id):
             "mesh": mesh_data,
             "wikidata": wikidata_data
         },
-        "inferred": {}  # TODO
+        "inferred": {}
     }
