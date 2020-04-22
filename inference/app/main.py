6ac491ec183d9a4cbc2cc9da871210f7ba9c542f
harrisonpim <harrison.stroma@gmail.com>
2020-04-22 10:58:25 +0100

    add endpoint for wikidata ids

--- inference/app/main.py
+++ inference/app/main.py
@@ -3,7 +3,8 @@ import logging
 
 from fastapi import FastAPI, HTTPException
 
-from .src.aggregate import aggregate_lc_names_data, aggregate_mesh_data
+from .src.aggregate import (aggregate_lc_names, aggregate_mesh,
+                            aggregate_wikidata)
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +12,7 @@ logger = logging.getLogger(__name__)
 logger.info("Starting API")
 app = FastAPI(
     title="Concepts Enhancer",
-    description="One stop shop for sanitising and enhancing concepts with wikidata",
+    description="One-stop-shop for sanitizing and enhancing concepts with wikidata",
 )
 logger.info("API started, awaiting requests")
 
@@ -19,26 +20,36 @@ logger.info("API started, awaiting requests")
 @app.get("/lc-names/{lc_names_id}")
 def lc_names_endpoint(lc_names_id: str):
     try:
-        response = aggregate_lc_names_data(lc_names_id)
+        response = aggregate_lc_names(lc_names_id)
+        logger.info(f"Aggregated concept data for lc_names ID: {lc_names_id}")
     except ValueError as e:
         error_string = str(e)
         logger.error(error_string)
         raise HTTPException(status_code=404, detail=error_string)
-
-    logger.info(f"aggregated concept data for lc_names ID: {lc_names_id}")
     return response
 
 
 @app.get("/mesh/{mesh_id}")
 def mesh_endpoint(mesh_id: str):
     try:
-        response = aggregate_mesh_data(mesh_id)
+        response = aggregate_mesh(mesh_id)
+        logger.info(f"Aggregated concept data for MeSH ID: {mesh_id}")
     except ValueError as e:
         error_string = str(e)
         logger.error(error_string)
         raise HTTPException(status_code=404, detail=error_string)
+    return response
 
-    logger.info(f"aggregated concept data for MeSH ID: {mesh_id}")
+
+@app.get("/wikidata/{wikidata_id}")
+def wikidata_endpoint(wikidata_id: str):
+    try:
+        response = aggregate_wikidata(wikidata_id)
+        logger.info(f"Aggregated concept data for wikidata ID: {wikidata_id}")
+    except ValueError as e:
+        error_string = str(e)
+        logger.error(error_string)
+        raise HTTPException(status_code=404, detail=error_string)
     return response
 
 
