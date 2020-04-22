fa57d884f05ce1ca44b73a5c2fa1b21f69ea3b5f
harrisonpim <harrison.stroma@gmail.com>
2020-04-22 10:57:54 +0100

    add aggregation from wikidata id

--- inference/app/src/aggregate.py
+++ inference/app/src/aggregate.py
@@ -8,58 +8,81 @@ from .exact.wikidata import get_wikidata_data
 log = logging.getLogger(__name__)
 
 
-def aggregate_lc_names_data(lc_names_id):
+def aggregate_lc_names(lc_names_id):
     response = {
-        'exact': {},
-        'inferred': {}
+        "exact": {},
+        "inferred": {}
     }
-    response['exact']['lc_names'] = get_lc_names_data(lc_names_id)
+    response["exact"]["lc_names"] = get_lc_names_data(lc_names_id)
     log.info(f"Got data from lc_names for ID: {lc_names_id}")
 
     try:
         wikidata_id = lc_names_id_to_wikidata_id(lc_names_id)
         log.info(
-            f'Found a link from lc_names ID: {lc_names_id} to wikidata ID: {wikidata_id}'
+            f"Found a link from lc_names ID: {lc_names_id} to wikidata ID: {wikidata_id}"
         )
     except ValueError:
+        wikidata_id = None
         log.info(f"Couldn't find a wikidata record for ID: {lc_names_id}")
 
     if wikidata_id:
-        response['exact']['wikidata'] = get_wikidata_data(wikidata_id)
+        response["exact"]["wikidata"] = get_wikidata_data(wikidata_id)
         log.info(f"Got data from wikidata for ID: {wikidata_id}")
 
         alt_source_ids = wikidata_id_to_alt_source_ids(wikidata_id)
-        if alt_source_ids['mesh']:
-            mesh_id = alt_source_ids['mesh']
-            response['exact']['mesh'] = get_mesh_data(mesh_id)
+        if alt_source_ids["mesh"]:
+            mesh_id = alt_source_ids["mesh"]
+            response["exact"]["mesh"] = get_mesh_data(mesh_id)
 
     return response
 
 
-def aggregate_mesh_data(mesh_id):
+def aggregate_mesh(mesh_id):
     response = {
-        'exact': {},
-        'inferred': {}
+        "exact": {},
+        "inferred": {}
     }
 
-    response['exact']['mesh'] = get_mesh_data(mesh_id)
+    response["exact"]["mesh"] = get_mesh_data(mesh_id)
     log.info(f"Got data from MeSH for ID: {mesh_id}")
 
     try:
         wikidata_id = mesh_id_to_wikidata_id(mesh_id)
         log.info(
-            f'Found a link from mesh ID: {mesh_id} to wikidata ID: {wikidata_id}'
+            f"Found a link from mesh ID: {mesh_id} to wikidata ID: {wikidata_id}"
         )
     except ValueError:
+        wikidata_id = None
         log.info(f"Couldn't find a wikidata record for ID: {mesh_id}")
 
     if wikidata_id:
-        response['exact']['wikidata'] = get_wikidata_data(wikidata_id)
+        response["exact"]["wikidata"] = get_wikidata_data(wikidata_id)
         log.info(f"Got data from wikidata for ID: {wikidata_id}")
 
         alt_source_ids = wikidata_id_to_alt_source_ids(wikidata_id)
-        if alt_source_ids['lc_names']:
-            lc_names_id = alt_source_ids['lc_names']
-            response['exact']['lc_names'] = get_lc_names_data(lc_names_id)
+        if alt_source_ids["lc_names"]:
+            lc_names_id = alt_source_ids["lc_names"]
+            response["exact"]["lc_names"] = get_lc_names_data(lc_names_id)
+
+    return response
+
+
+def aggregate_wikidata(wikidata_id):
+    response = {
+        "exact": {},
+        "inferred": {}
+    }
+
+    response["exact"]["wikidata"] = get_wikidata_data(wikidata_id)
+    log.info(f"Got data from wikidata for ID: {wikidata_id}")
+
+    alt_source_ids = wikidata_id_to_alt_source_ids(wikidata_id)
+    print(alt_source_ids)
+    if alt_source_ids["lc_names"]:
+        lc_names_id = alt_source_ids["lc_names"]
+        response["exact"]["lc_names"] = get_lc_names_data(lc_names_id)
+    if alt_source_ids["mesh"]:
+        mesh_id = alt_source_ids["mesh"]
+        response["exact"]["mesh"] = get_mesh_data(mesh_id)
 
     return response
