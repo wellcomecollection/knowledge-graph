6bac375625b888b960ec97f969582f8a58a66196
harrisonpim <harrison.stroma@gmail.com>
2022-01-10 13:33:03 +0000

    move stories work to repo root

--- enricher/app/src/wikidata/__init__.py
+++
@@ -1,2 +0,0 @@
-from .api import get_wikidata_api_response, get_wikidata_data  # noqa: F401
-from .sparql import get_wikidata_id_from_prop  # noqa: F401
--- graph_query_api/README.md
+++
@@ -1,7 +0,0 @@
-# Query API
-
-Queries a pre-populated graph store to return variant names
-
-hit
-
-- `http://localhost?query=something`
