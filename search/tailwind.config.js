8a2c9caf706a90a82a3637960ed5f733f26e3aa7
harrisonpim <harrison.stroma@gmail.com>
2022-02-21 14:31:46 +0000

    add the prototype colour palette

--- search/tailwind.config.js
+++ search/tailwind.config.js
@@ -3,12 +3,20 @@ module.exports = {
     './components/**/*.{html,js,ts,jsx,tsx}',
     './pages/**/*.{html,js,ts,jsx,tsx}',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: '#C88F7E',
+        salmon: '#E6B692',
+        blue: '#729CB2',
+        pink: '#E0B3BF',
+        green: '#AAAA81',
+        yellow: '#DFD76E',
+        black: '#131313',
+        brown: '#C7A879',
+        paper: { 1: '#FBF6E9', 2: '#F0EADA', 3: '#DED5BF' },
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
