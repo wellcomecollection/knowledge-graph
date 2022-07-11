dda8405f3ce7c067417d019fddb98d846e5657b9
harrisonpim <hello@harrisonpim.com>
2022-07-11 19:38:04 +0100

    api endpoints for works, subjects and people

---
+++ search/pages/api/works/[id].ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getClient, getWork } from '../../../services/elasticsearch'
+
+export default async function searchEndpoint(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    try {
+      const { id } = req.query
+      const client = getClient()
+      const work = await getWork(client, id)
+      res.status(200).json({
+        ...work,
+      })
+    } catch {
+      res.status(500).json({ error: 'Unable to query' })
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' })
+  }
+}
