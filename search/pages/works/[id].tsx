a9193c0e8cf496dc0254f2dc4a2ebbacfd793466
harrisonpim <hello@harrisonpim.com>
2022-07-25 14:02:33 +0100

    add a table comparing original and enriched concepts

--- search/pages/works/[id].tsx
+++ search/pages/works/[id].tsx
@@ -9,14 +9,16 @@ import { useState } from 'react'
 
 type Props = {
   work: Work
-  originalWorkSubjects: {
-    label: string
-    id: string
-    type: string
-    concepts: {
+  subjectComparisonTable: {
+    original: {
+      label: string
+      id: string
+    }
+    enriched: {
       label: string
       type: string
-    }[]
+      id: string
+    }
   }[]
 }
 
@@ -34,11 +36,32 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const originalWork: wecoApiWork = await fetch(
     `https://api.wellcomecollection.org/catalogue/v2/works/${workId}?include=subjects`
   ).then((res) => res.json())
+
   const originalWorkSubjects = originalWork.subjects
+    ? originalWork.subjects
+    : []
+  const subjectComparisonTable = originalWorkSubjects.map((originalSubject) => {
+    const enrichedSubject = work.concepts.find(
+      (enrichedSubject) =>
+        enrichedSubject.originalLabel === originalSubject.label
+    )
+    return {
+      original: {
+        label: originalSubject.label ? originalSubject.label : '',
+        id: originalSubject.id ? originalSubject.id : '',
+      },
+      enriched: {
+        label: enrichedSubject ? enrichedSubject.label : '',
+        id: enrichedSubject ? enrichedSubject.id : '',
+        type: enrichedSubject ? enrichedSubject.type : '',
+      },
+    }
+  })
+
   return {
     props: {
       work,
-      originalWorkSubjects,
+      subjectComparisonTable,
     },
   }
 }
@@ -46,7 +69,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
 const WorkPageTabs = ['Catalogue details', 'View', 'Related']
 type WorkPageTabType = typeof WorkPageTabs[number]
 
-const WorkPage: NextPage<Props> = ({ work, originalWorkSubjects }) => {
+const WorkPage: NextPage<Props> = ({ work, subjectComparisonTable }) => {
   const [selectedTab, setSelectedTab] = useState(
     'Catalogue details' as WorkPageTabType
   )
@@ -69,7 +92,7 @@ const WorkPage: NextPage<Props> = ({ work, originalWorkSubjects }) => {
               {work.contributors.length > 0 &&
                 work.published.length > 0 &&
                 ' | '}
-              {work.contributors.map((c) => c.name).join(', ')}
+              {work.contributors.map((c) => c.label).join(', ')}
             </p>
 
             <ul className="flex divide-gray-400 border-b pt-4">
@@ -92,23 +115,26 @@ const WorkPage: NextPage<Props> = ({ work, originalWorkSubjects }) => {
         </div>
         <div className="mx-auto space-y-8 px-5 lg:w-3/4">
           <h2 className="font-sans font-light">About this work</h2>
-          <div>
-            <p className="font-bold">Description</p>
-            <p>{work.description}</p>
-          </div>
-          <div>
-            <p className="font-bold">Contributors</p>
-            <ul className="capitalize">
-              {work.contributors &&
-                work.contributors.map((contributor) => (
+          {work.description && (
+            <div>
+              <p className="font-bold">Description</p>
+              <p>{work.description}</p>
+            </div>
+          )}
+          {work.contributors.length > 0 && (
+            <div>
+              <p className="font-bold">Contributors</p>
+              <ul className="capitalize">
+                {work.contributors.map((contributor) => (
                   <li key={contributor.id}>
-                    <a className="" href={`/people/${contributor.id}`}>
-                      {contributor.name}
+                    <a href={`/people/${contributor.id}`}>
+                      {contributor.label}
                     </a>
                   </li>
                 ))}
-            </ul>
-          </div>
+              </ul>
+            </div>
+          )}
           <div>
             <p className="font-bold">Subjects</p>
             <ul className="flex flex-wrap gap-x-2 pt-4">
@@ -117,10 +143,10 @@ const WorkPage: NextPage<Props> = ({ work, originalWorkSubjects }) => {
                   <a
                     className="w-100 rounded-full border border-gray-400 py-2 px-3 text-sm capitalize no-underline"
                     href={`/${
-                      concept.type == 'concept' ? 'subjects' : 'people'
+                      concept.type == 'subject' ? 'subjects' : 'people'
                     }/${concept.id}`}
                   >
-                    {concept.name}
+                    {concept.label}
                   </a>
                 </li>
               ))}
@@ -128,20 +154,42 @@ const WorkPage: NextPage<Props> = ({ work, originalWorkSubjects }) => {
           </div>
           <div>
             <p className="font-bold">
-              Subjects on the{' '}
+              Subjects compared to the{' '}
               <a href={`https://wellcomecollection.org/works/${work.id}`}>
                 original work page
               </a>
             </p>
-            <ul className="flex flex-wrap gap-x-2 pt-4">
-              {originalWorkSubjects.map((subject) => (
-                <li key={subject.id} className="pb-6">
-                  <span className="w-100 rounded-full border border-gray-400 py-2 px-3 text-sm capitalize no-underline">
-                    {subject.label}
-                  </span>
-                </li>
-              ))}
-            </ul>
+            <div className="pt-4">
+              <table className="table-fixed ">
+                <thead>
+                  <tr className="border-b text-left">
+                    <th className="pl-1 pr-3">Original</th>
+                    <th className="pl-1 pr-3">Enriched</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {subjectComparisonTable.map((subject, i) => (
+                    <tr
+                      key={subject.original.id ? subject.original.id : i}
+                      className={i % 2 == 0 ? 'bg-gray-100' : ''}
+                    >
+                      <td className="pl-1 pr-3">{subject.original.label}</td>
+                      <td className="pl-1 pr-3 capitalize">
+                        <a
+                          href={`/${
+                            subject.enriched.type == 'subject'
+                              ? 'subjects'
+                              : 'people'
+                          }/${subject.enriched.id}`}
+                        >
+                          {subject.enriched.label}
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </Layout>
